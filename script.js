b6eb5f938ccea40696e80e2efaf94d02da76e64f
stevekindstrand <stevekindstrand@gmail.com>
2021-12-03 20:13:02 +0100

    second commit, växlar mellan logga in och registrering on click

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+let linkLog = document.getElementById("linkLog");
+let linkReg = document.getElementById("linkReg");
+
 registerForm.style.display = "none";
+
+linkLog.addEventListener("click", ()=>{
+    loginForm.style.display = "none";
+    registerForm.style.display = "flex";
+});
+
+linkReg.addEventListener("click", ()=>{
+    registerForm.style.display = "none";
+    loginForm.style.display = "flex";
+});
