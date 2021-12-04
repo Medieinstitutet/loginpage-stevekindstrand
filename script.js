f13e8f6faa595aa66e4a43501016a0af419bae58
stevekindstrand <stevekindstrand@gmail.com>
2021-12-04 13:34:59 +0100

    4th commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Lägg till alla element
+//Hämtar element från index
 let loginInput = document.getElementById("loginInput");
 let loginPwInput = document.getElementById("loginPwInput");
 let loginBtn = document.getElementById("loginBtn");
@@ -12,7 +12,11 @@ let linkReg = document.getElementById("linkReg");
 let divLoggedIn = document.getElementById("divLoggedIn");
 let divTryAgain = document.getElementById("divTryAgain");
 
-//Döljer element
+//Skapar nya element som hämtas när man loggar in eller skriver in fel användarnamn/lösenord
+let h2 = document.createElement("h2");
+let logoutBtn = document.createElement("button");
+
+//Döljer element (Ska byta till innerHTML om jag hinner)
 registerForm.style.display = "none";
 divLoggedIn.style.display = "none";
 divTryAgain.style.display = "none";
@@ -28,49 +32,71 @@ linkReg.addEventListener("click", ()=>{
     loginForm.style.display = "flex";
 });
 
-//Logga in och logga
-let h2 = document.createElement("h2");
-let logoutBtn = document.createElement("button");
+//Skapar en "logga ut" knapp 
+logoutBtn.addEventListener("click", ()=>{
+    divLoggedIn.style.display = "none";
+    loginForm.style.display = "flex";
+    location.reload();
+});
 
+//Skapar tre fungerande inloggningar
+let objUsers = [
+    {
+        usernameInput: "janne",
+        passwordInput: "test"
+    },
+    {
+        usernameInput: "admin",
+        passwordInput: "admin"     
+    },
+    {
+        usernameInput: "0",
+        passwordInput: "0"     
+    }
+];
+
+/*--------------------
+Logga in och logga ut
+--------------------*/
 loginBtn.addEventListener("click", (event)=>{
     event.preventDefault();
 
     let usernameInput = loginInput.value;
     let passwordInput = loginPwInput.value;
-
-    if(usernameInput == "1" && passwordInput == "1"){
-        loginForm.style.display = "none";
-        divLoggedIn.style.display = "flex";
-
-        localStorage.setItem(usernameInput, passwordInput);
-
-        divLoggedIn.append(h2);
-        h2.innerHTML = "Välkommen, " + usernameInput + "!<br><br>";
-
-        divLoggedIn.append(logoutBtn);
-        logoutBtn.innerHTML = "Logga ut";
-
-        logoutBtn.addEventListener("click", ()=>{
-            divLoggedIn.style.display = "none";
-            loginForm.style.display = "flex";
-            location.reload();
-        });
-    }else{
-        loginForm.style.display = "none";
-        divTryAgain.style.display = "flex";
-
-        divTryAgain.append(h2);
-        h2.innerHTML = "Fel användarnamn eller lösenord, försök igen<br><br>";
-
-       divTryAgain.append(logoutBtn);
-        logoutBtn.innerHTML = "försök igen";
-
-        logoutBtn.addEventListener("click", ()=>{
-            divTryAgain.style.display = "none";
-            loginForm.style.display = "flex";
-            location.reload();
-        });
-    };
-});
-
+    
+    for(i = 0; objUsers.length; i++){
+        if(usernameInput == objUsers[i].usernameInput && passwordInput == objUsers[i].passwordInput){
+            loginForm.style.display = "none";
+            divLoggedIn.style.display = "flex";
+        
+            //localStorage.setItem(usernameInput, passwordInput);
+        
+            divLoggedIn.append(h2);
+            h2.innerHTML = "Välkommen, " + usernameInput + "!<br><br>";
+        
+            divLoggedIn.append(logoutBtn);
+            logoutBtn.innerHTML = "Logga ut";
+            return;
+        
+        }else{
+            loginForm.style.display = "none";
+            divTryAgain.style.display = "flex";
+        
+            divTryAgain.append(h2);
+            h2.innerHTML = "Fel användarnamn eller lösenord, försök igen<br><br>";
+        
+           divTryAgain.append(logoutBtn);
+            logoutBtn.innerHTML = "försök igen";
+        }};
+    });  
+    
 //Registrera ett konto
+
+// // Storing data:
+// const myJSON = JSON.stringify(objUsers);
+// localStorage.setItem("users", myJSON);
+
+// // Retrieving data:
+// let text = localStorage.getItem("users");
+// let obj = JSON.parse(text);
+// document.getElementById("divLoggedIn").innerHTML = objUsers.usernameInput;
