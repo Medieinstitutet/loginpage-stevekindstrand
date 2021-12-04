9aa4f42424f79a4600cb38ef10501aeec1fb874b
stevekindstrand <stevekindstrand@gmail.com>
2021-12-04 17:07:05 +0100

    5th commit

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ let loginPwInput = document.getElementById("loginPwInput");
 let loginBtn = document.getElementById("loginBtn");
 let linkLogin = document.getElementById("linkLogin");
 
-let regInput = document.getElementById("regInput");
-let regPwInput = document.getElementById("regPwInput");
-let regBtn = document.getElementById("regBtn");
-let linkReg = document.getElementById("linkReg");
+let regInput = document.getElementById("regInput"); //Används ej än
+let regPwInput = document.getElementById("regPwInput"); //Används ej än
+let regBtn = document.getElementById("regBtn"); //Används ej än
+let linkReg = document.getElementById("linkReg"); 
 
 let divLoggedIn = document.getElementById("divLoggedIn");
 let divTryAgain = document.getElementById("divTryAgain");
@@ -16,7 +16,7 @@ let divTryAgain = document.getElementById("divTryAgain");
 let h2 = document.createElement("h2");
 let logoutBtn = document.createElement("button");
 
-//Döljer element (Ska byta till innerHTML om jag hinner)
+//Döljer element
 registerForm.style.display = "none";
 divLoggedIn.style.display = "none";
 divTryAgain.style.display = "none";
@@ -37,9 +37,11 @@ logoutBtn.addEventListener("click", ()=>{
     divLoggedIn.style.display = "none";
     loginForm.style.display = "flex";
     location.reload();
+
+    localStorage.clear(); //Rensar local storage när man trycker på "logga ut"
 });
 
-//Skapar tre fungerande inloggningar
+//Skapar tre inloggningar
 let objUsers = [
     {
         usernameInput: "janne",
@@ -66,18 +68,23 @@ loginBtn.addEventListener("click", (event)=>{
     
     for(i = 0; objUsers.length; i++){
         if(usernameInput == objUsers[i].usernameInput && passwordInput == objUsers[i].passwordInput){
+            
             loginForm.style.display = "none";
             divLoggedIn.style.display = "flex";
-        
-            //localStorage.setItem(usernameInput, passwordInput);
-        
+  
             divLoggedIn.append(h2);
             h2.innerHTML = "Välkommen, " + usernameInput + "!<br><br>";
         
             divLoggedIn.append(logoutBtn);
             logoutBtn.innerHTML = "Logga ut";
+
+            localStorage.setItem("username", usernameInput); //Visar inloggad användare i local storage
+
+            let userName = localStorage.getItem("username"); //Hämtar inloggad användare från local storage till console
+            console.log(userName);
+
             return;
-        
+
         }else{
             loginForm.style.display = "none";
             divTryAgain.style.display = "flex";
@@ -88,15 +95,12 @@ loginBtn.addEventListener("click", (event)=>{
            divTryAgain.append(logoutBtn);
             logoutBtn.innerHTML = "försök igen";
         }};
-    });  
-    
-//Registrera ett konto
+    });
 
-// // Storing data:
-// const myJSON = JSON.stringify(objUsers);
-// localStorage.setItem("users", myJSON);
+/*--------------------
+Registrera en användare
+--------------------*/
 
-// // Retrieving data:
-// let text = localStorage.getItem("users");
-// let obj = JSON.parse(text);
-// document.getElementById("divLoggedIn").innerHTML = objUsers.usernameInput;
+//Sparar existerande användare i local storage
+const objJSON = JSON.stringify(objUsers);
+localStorage.setItem("all users", objJSON);
